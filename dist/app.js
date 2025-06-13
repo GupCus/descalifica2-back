03ac56eb60f3dff83d1ea97db34982617809d39e
franfigit <ffigueroa@frro.utn.edu.ar>
2025-06-13 10:24:14 -0300

    Pusheo base del proyecto

---
+++ dist/app.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+app.use(express.json());
+app.use('/', (req, res) => {
+    res.send("Hello");
+});
+app.listen(3000, () => {
+    console.log('Corriendo');
+});
+//# sourceMappingURL=app.js.map
