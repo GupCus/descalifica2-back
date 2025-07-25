e80324831d563c3d62a18c2957126875b55b968d
franfigit <ffigueroa@frro.utn.edu.ar>
2025-07-25 20:19:01 -0300

    Avanzo casi nada

---
+++ src/temporada/temporada.controller.ts
@@ -0,0 +1,48 @@
+
+
+
+const repository = new temporadaRepository()
+
+function sanitizePilotoInput(req: Request, res: Response, next: NextFunction){ //Response, Request y NextFunction son de express
+  req.body.sanitizedInput = {
+      name: req.body.name,
+      team: (new EscuderiaRepository).findOne({id:req.body.team}),
+      num: req.body.num,
+      nationality: req.body.nationality,
+      role: req.body.role,
+      id: req.params.id
+  }
+   Object.keys(req.body.sanitizedInput).forEach(key => { //borra todos los atributos que no nos pasaron en el PATCH, evitamos errores
+      if(req.body.sanitizedInput[key] === undefined){delete req.body.sanitizedInput[key]}
+   })
+  next()
+}
+
+//get todos los pilotos
+function findAll(req:Request,res:Response){
+  res.status(500).json({ message: 'Not implemented.'})
+}
+
+//get para un piloto en específico
+function findOne(req:Request,res:Response) { 
+  res.status(500).json({ message: 'Not implemented.'})
+}
+
+//post un nuevo piloto
+function add(req:Request,res:Response){
+  res.status(500).json({ message: 'Not implemented.'})
+}
+
+//put&patch de piloto
+function update(req:Request,res:Response) { 
+res.status(500).json({ message: 'Not implemented.'})
+}
+
+//Aunque este definida en el repository con un parametro {id: string} de esta forma tenemos la versatilidad de que manden tanto asi como el character entero
+function remove(req:Request,res:Response){ 
+  res.status(500).json({ message: 'Not implemented.'})
+}
+
+
+
+export {sanitizePilotoInput, findAll, findOne, add, update, remove}
