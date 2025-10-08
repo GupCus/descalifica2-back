5e2aaf554d53c437866febbd7ef28312dbe6bb6a
Francisco Alejandro Figueroa <ffigueroa@frro.utn.edu.ar>
2025-10-07 21:16:06 -0300

    Eliminadas algunas redundancias, eliminado título de Blogpost (baseEntity cuenta con name).

--- src/categoria/categoria.entity.ts
+++ src/categoria/categoria.entity.ts
@@ -1,26 +1,34 @@
-import { Collection, Entity, ManyToMany, OneToMany, PrimaryKey, Property, Cascade } from "@mikro-orm/core";
+import {
+  Collection,
+  Entity,
+  ManyToMany,
+  OneToMany,
+  PrimaryKey,
+  Property,
+  Cascade,
+} from "@mikro-orm/core";
 import { Escuderia } from "../escuderia/escuderia.entity.js";
 import { baseEntity } from "../shared/baseEntity.entity.js";
 import { Temporada } from "../temporada/temporada.entity.js";
 import { Piloto } from "../piloto/piloto.entity.js";
 
-
 @Entity()
-export class Categoria extends baseEntity{
-    @Property({nullable: false})
-    name!: string
+export class Categoria extends baseEntity {
+  @Property({ nullable: true })
+  description?: string;
 
-    @Property({nullable: true})
-    description?: string
+  @OneToMany(() => Piloto, (piloto) => piloto.racing_series, {
+    cascade: [Cascade.ALL],
+    nullable: true,
+  })
+  drivers = new Collection<Piloto>(this);
 
-    @OneToMany(() => Piloto, (piloto) => piloto.racing_series, {cascade:[Cascade.ALL], nullable: true})
-    drivers = new Collection<Piloto>(this)
+  @OneToMany(() => Escuderia, (escuderia) => escuderia.categoria)
+  escuderias = new Collection<Escuderia>(this);
 
-    @OneToMany(() => Escuderia, (escuderia) => escuderia.categoria)
-    escuderias = new Collection<Escuderia>(this);
-
-    @OneToMany(() => Temporada, (temporada) => temporada.racing_series, {cascade:[Cascade.ALL], nullable: true})
-    seasons = new Collection<Temporada>(this)
-
+  @OneToMany(() => Temporada, (temporada) => temporada.racing_series, {
+    cascade: [Cascade.ALL],
+    nullable: true,
+  })
+  seasons = new Collection<Temporada>(this);
 }
-
