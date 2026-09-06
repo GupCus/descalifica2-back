ac1c37d89f854706ddbbda88bf7459c16b2bca41
Agus <abarrosobol@frro.utn.edu.ar>
2026-09-06 17:13:23 -0300

    agregado servicio de telegram, el cual dado un usuario se crea temporalmente un codigo de validacion en el campo telegram_id, luego si este usuario envia al bot /start codigo lo valida, al estar validado el usuario se enviaran los mensajes masivos a este TODO: crear la logica del codigo temporal al añadir el campo usuariotelegram al usuario y crear un endpoint para indicar el estado de la validacion al front

--- src/usuario/usuario.entity.ts
+++ src/usuario/usuario.entity.ts
@@ -4,11 +4,11 @@ import {
   Entity,
   OneToMany,
   Property,
-} from '@mikro-orm/core';
-import { baseEntity } from '../shared/baseEntity.entity.js';
-import { Blogpost } from '../blogpost/blogpost.entity.js';
-import bcrypt from 'bcrypt';
-import { ComentarioPost } from '../comentariopost/comentario.entity.js';
+} from "@mikro-orm/core";
+import { baseEntity } from "../shared/baseEntity.entity.js";
+import { Blogpost } from "../blogpost/blogpost.entity.js";
+import bcrypt from "bcrypt";
+import { ComentarioPost } from "../comentariopost/comentario.entity.js";
 
 @Entity()
 export class Usuario extends baseEntity {
@@ -36,6 +36,8 @@ export class Usuario extends baseEntity {
   avatar?: string;
   @Property({ nullable: true, unique: true })
   telegram_username?: string;
+  @Property({ nullable: true, unique: true })
+  telegram_id?: string;
   @OneToMany(() => Blogpost, (blogpost) => blogpost.author)
   posts = new Collection<Blogpost>(this);
   @OneToMany(() => ComentarioPost, (comentario) => comentario.author)
